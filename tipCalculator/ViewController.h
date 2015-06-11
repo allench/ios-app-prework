cdafbdb6ccb03bb1591995d19fff5402152283cd
allench <allench@yahoo-inc.com>
2015-06-11 21:11:08 +0800

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Allen Chiang on 6/11/15.
+//  Copyright (c) 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
